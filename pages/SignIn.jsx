13a6db934d4983246a854561415809f292c03981
Thibault FONTAINE <127660064+ThibaultFontaine@users.noreply.github.com>
2024-10-28 15:35:11 +0100

    Setup routing and first page (#20)

---
+++ pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function SignIn() {
+  return (
+    <View>
+      <Text>
+        Sign In
+      </Text>
+    </View>
+  );
+}
+
