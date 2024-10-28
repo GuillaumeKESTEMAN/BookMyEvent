05a3483ee45c23e2eb63314b05e60ee8c21909d3
Thibault Fontaine <thibault.fontaine@protonmail.com>
2024-10-28 14:39:58 +0100

    Setup routing and first page

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { name as appName } from './app.json';
+
+import SignIn from './pages/SignIn';
+
+const Tab = createBottomTabNavigator();
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
 	return (
 		<PaperProvider>
-			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
-			</View>
+			<NavigationContainer>
+				<Tab.Navigator
+					initialRouteName='login'
+				>
+					<Stack.Screen
+						name='login'
+						component={SignIn}
+					/>
+				</Tab.Navigator>
+			</NavigationContainer>
 		</PaperProvider>
 	);
 }
@@ -15,10 +31,4 @@ export default function App() {
 AppRegistry.registerComponent(appName, () => App);
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
 });
