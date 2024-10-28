8540ddd81432ad9252b9acc1e7134fe19e8f4d4f
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2024-10-28 12:34:12 +0100

    chore: install react-native-paper and react-native-vector-icons

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+import { PaperProvider } from 'react-native-paper';
+import { name as appName } from './app.json';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<PaperProvider>
+			<View style={styles.container}>
+				<Text>Open up App.js to start working on your app!</Text>
+			</View>
+		</PaperProvider>
+	);
 }
 
+AppRegistry.registerComponent(appName, () => App);
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
